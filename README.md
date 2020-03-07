2681130bbc141e40dae54504caa7e337f64c79dd
Morgan Nelson <morgan.nelson@gmail.com>
2020-03-07 17:20:19 -0600

    Github repo cleanup

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Unit_8_Preso
  MSDS 6306 "Doing Data Science" 1st project for Unit 8/9
+ 
+ This project consists of:
+ 
+ * Unit_8_Preso.Rproj
+    The r project file that will load the environment into r
+    
+ * Unit 8 Case Study_Nelson_Hedge.pptx
+    The output PowerPoint file that contains the presentation deck
+    
+ * Slide_Deck.Rmd
+    The main "working" file that contains all r commands and environment.  This file may install libraries (all code that installs libraries is commented out and must be uncommented in order to modify your system).  This is the file that will knit together to form the basis of this project.
+    
+ * Beers.csv
+ * Breweries.csv
+    The original source data files required for this project
+    
+ * beer_glass_background.jpg
+    The image file used as the background for the graphs in the project
+    
+ * The misc/ directory
+    Contains various files useful to the developers during work on the project.  Not used in the project directly, can be ignored.
+    
+ * Files that begin with "Slide_Deck" that aren't "Slide_Deck.Rmd"
+    Various output from knitting the Rmd file into different formats:
+     * pdf
+     * MS-Word
+     * LaTeX
+     * HTML
+     * etc
---
+++ misc/MHedge.R
@@ -0,0 +1,131 @@
+library(funModeling) 
+library(Hmisc)
+
+# basic EDA commands to look at Beer data
+  glimpse(Beers)
+  df_status(Beers)
+  freq(Beers) 
+  profiling_num(Beers)
+  plot_num(Beers)
+  describe(Beers)
+
+# basic EDA commands to look at Breweries data
+  glimpse(Breweries)
+  df_status(Breweries)
+  freq(Breweries) 
+  profiling_num(Breweries)
+  plot_num(Breweries)
+  describe(Breweries)
+  
+  str(Beers)
+  summary(Beers)
+  head(Beers)
+  count(Beers)
+  length(Beers)
+
+  Beers %>% 
+    select(ABV,IBU) %>% 
+    ggpairs()
+  
+  summary(Beers)
+  str(Breweries)
+  Breweries
+
+  # counts number of breweries in datafile
+total_BR=Breweries %>%
+  group_by("State") %>% 
+  summarise(count=n())
+
+total_BR
+
+
+
+p = Breweries %>% 
+  ggplot(aes(y=total_BR)) +
+  geom_boxplot() +
+  ggplotly(Breweries$State)
+
+Beers
+Breweries
+gg_miss_var(Beers)
+
+All_Beer <-merge(Breweries, Beers, by.x="Brew_ID", by.y = "Brewery_id")
+All_Beer
+
+head(All_Beer)
+
+str(Beers)
+str(Breweries)
+str(All_Beer)
+
+
+All_Beer %>% 
+  select(ABV,IBU,State) %>% 
+  ggpairs(aes(color="State"), cardinality_threshold = NULL, bins)
+
+z = All_Beer %>% 
+  ggplot(aes(x="state")) +
+  geom_histogram() +
+  ggplotly()
+
+gg_miss_var(All_Beer)
+
+write.csv(All_Beer,"All_Beer.csv")
+gg_miss_var(brewery_count_by_state)
+brewery_count_by_state %>% ggplot(aes(x=brewery_count_by_state$count)) +
+  geom_histogram() +
+  ggplotly()
+
+str(All_Beer)
+head(All_Beer)
+names(All_Beer)
+
+names(All_Beer)[2] <- "Brewery"
+names(All_Beer)[5] <- "Beer"
+
+All_Beer %>% 
+  select("State","Brewery") %>% 
+  group_by("state") %>% 
+  summarize("Brewery")
+  
+ST_tot <-count(All_Beer, vars = "State") # count ("st_total")
+ST_tot
+summary(ST_tot)
+
+
+
+summarize("st_tot")
+summary(All_Beer$ABV)
+boxplot(All_Beer$ABV)
+
+cor("st_tot")
+
+
+head(All_Beer,6)
+tail(All_Beer,6)
+table("st_tot")
+
+
+max(All_Beer$ABV,na.rm = TRUE)
+
+max(All_Beer$IBU,na.rm = TRUE)
+
+summary(All_Beer$ABV)
+
+
+mean(All_Beer$ABV, na.rm=TRUE)
+sd(All_Beer$ABV, na.rm=TRUE)
+
+
+mean(All_Beer$IBU, na.rm=TRUE)
+summary(All_Beer$IBU,na.rm=TRUE)
+sd(All_Beer$IBU,na.rm=TRUE)
+boxplot(All_Beer$IBU, x=0:20)
+boxplot(All_Beer$IBU)
+
+head(All_Beer)
+
+max_abv <- All_Beer %>% 
+    select(All_Beer, ABV,IBU,State) %>%
+  rev(sort(ABV))
+  
